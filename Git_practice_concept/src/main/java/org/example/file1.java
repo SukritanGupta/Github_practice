b4963f7a0cce56e25753e4b339b0b74691054348
SukritanGupta_fico <sukritangupta@fico.com>
2024-08-14 12:09:44 +0530

    commit2 of branch2

---
+++ Git_practice_concept/src/main/java/org/example/file1.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class file1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to code world");
+        System.out.println("Welcome");
+        System.out.println("Sukritan Gupta");
+    }
+}
---
+++ Git_practice_concept/src/main/java/org/example/file2.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class file2 {
+    public static void main(String[] args) {
+        System.out.println("welcome to code world1");
+    }
+}
