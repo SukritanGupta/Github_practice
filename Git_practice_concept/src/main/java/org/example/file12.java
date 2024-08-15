2761f0b5d6518fc6ad8c6ad78bb7d1143ac6b025
SukritanGupta_fico <sukritangupta@fico.com>
2024-08-15 21:17:21 +0530

    create file12 in task/branch3

---
+++ Git_practice_concept/src/main/java/org/example/file12.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class file12 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Ruchikas world");
+        System.out.println("Welcome to Ruchikas world");
+    }
+}
