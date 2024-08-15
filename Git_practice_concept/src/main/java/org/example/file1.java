d71c6341cd0b8b971fda393127968ab9cb1303a7
SukritanGupta_fico <sukritangupta@fico.com>
2024-08-15 20:24:51 +0530

    commit4 of branch1

--- Git_practice_concept/src/main/java/org/example/file1.java
+++ Git_practice_concept/src/main/java/org/example/file1.java
@@ -4,5 +4,6 @@ public class file1 {
     public static void main(String[] args) {
         System.out.println("Welcome to code world");
         System.out.println("Welcome to sukritan gupta world where ruchika is queeen");
+        System.out.println("Sukritan");
     }
 }
